54cbdd92a8c97761ac95fe23aaed541adb21455f
Jonny Sueck <jonny.sueck@petco.com>
2024-04-16 20:17:55 -0400

    Added in a function to place a marker on the map where the user is located. When the user taps the marker, a getAddress function allows the user to see the address as a label.

--- app/src/main/java/com/kodeco/cityguide/MapsActivity.kt
+++ app/src/main/java/com/kodeco/cityguide/MapsActivity.kt
@@ -1,7 +1,13 @@
 package com.kodeco.cityguide
 
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -15,24 +21,28 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.kodeco.cityguide.databinding.ActivityMapsBinding
+import java.io.IOException
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Obtains the system sidebars and enters full screen
+        // Obtains the system sidebars and enter full screen
         val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
         windowInsetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
-
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -51,14 +61,65 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
      */
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val charlotte = LatLng(35.2270869, -80.8431267)
-        map.addMarker(MarkerOptions().position(charlotte).title("Marker in Charlotte, NC"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(charlotte))
         map.uiSettings.isZoomControlsEnabled = true
         map.setOnMarkerClickListener(this)
+        setUpMap()
     }
 
     override fun onMarkerClick(p0: Marker) = false
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request location if access fine location is not granted
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        map.isMyLocationEnabled = true
+        // Set the map to Terrain
+        map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if(location != null) {
+                // Set lastLocation to current location and zoom to area on map
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+    }
+
+    private fun placeMarkerOnMap(location: LatLng) {
+        val markerOptions = MarkerOptions().position(location)
+        val titleStr = getAddress(location)  // add these two lines
+        markerOptions.title(titleStr)
+        map.addMarker(markerOptions)
+    }
+
+    /**
+     * Converts a lat & lng to an address using the geocoder.
+     * This callback is triggered when a map marker is clicked.
+     * This is where we can add the address text to a marker label
+     */
+    private fun getAddress(latLng: LatLng): String {
+        val geocoder = Geocoder(this) // Turns lat&lng into an address or vice versa
+        val addresses: List<Address>?
+        val address: Address?
+        var addressText = ""
+
+        try {
+            // Request the address from the geocoder and set addressText to the address
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            if (!addresses.isNullOrEmpty()) {
+                address = addresses[0]
+                for (i in addresses.indices) {
+                    addressText += if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(i)
+                }
+            }
+        } catch (e: IOException) {
+            Log.e("MapsActivity", e.localizedMessage)
+        }
+
+        return addressText
+    }
+
 }
